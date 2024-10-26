8cb480cbda1517dc8aad83f527f19ac395187590
jiyuujin <jiyuujinunite@gmail.com>
2024-10-26 10:32:48 +0900

    fix: title

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NearbyLunch App (ランチマップアプリ)',
+      title: 'Lunch Nearby App (ランチマップアプリ)',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
